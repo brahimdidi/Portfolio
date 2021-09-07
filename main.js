b9af6bafcf0a516f417f9cf887e11c0077b7b650
brahim <brahimgb1team@gmail.com>
2021-09-07 22:36:12 +0000

    add popup page using main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ arrowRight3.src = 'template5Images/arrowRight.svg';
 
 // create the cancel button
 const cancelbtn = document.createElement('img');
-cancelbtn.src = 'template5Images/Icon - Cancel.svg';
+cancelbtn.src = 'template5Images/Icon-Cancel.svg';
 // start open function
 const open = () => {
   header.classList.add('header-active');
@@ -62,3 +62,73 @@ cancelbtn.addEventListener('click', cancel);
 ulLinks.addEventListener('click', () => { unfixHeader(); cancel(); });
 ulLinks2.addEventListener('click', () => { unfixHeader(); cancel(); });
 ulLinks3.addEventListener('click', () => { unfixHeader(); cancel(); });
+
+//popup detail project start
+const button = document.querySelectorAll('.btn2');
+const Modal = document.getElementById('Modal');
+const main = document.querySelector('.main');
+const footer = document.getElementById('footer');
+// cancelIcon Object
+const cancelIconInfos = {
+  src : "template5Images/Icon-Cancel.svg",
+  alt : "cancelIcon"
+}
+const Modalsection = document.createElement('section');
+Modalsection.classList.add('dis-none');
+Modalsection.innerHTML = `
+<div class="snapchoot-modal"><img src="template5Images/Snapshoot-PopupMobile.svg" alt="snapchoot-modal"></div>
+<div class="snapchoot-modal-desktop"><img src="template5Images/Snapshoot-Modal-Desktop.png" alt="snapchoot-modal"></div>
+<p class="modal-para-mobile">Lorem Ipsum is simply dummy text of the printing and typesetting industry
+. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+ when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum
+ is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown 
+ printer took a galley of type veris lapoa todoe.</p>
+ <p class="modal-para-desktop">Lorem Ipsum is simply dummy text of the printing and typesetting industry
+     . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+     when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
+      Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+       the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is
+        simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+       when an unknown printer took a galley of type and scramble.</p>
+ <div class="modal-ul">
+     <img src="template5Images/devider-langUl.png" class="devider-langUl" alt="devider">
+     <li class="ul-lang">html</li>
+     <img src="template5Images/devider-langUl.png" class="devider-langUl" alt="devider">
+     <li class="ul-lang">Ruby On rails</li>
+     <img src="template5Images/devider-langUl.png" class="devider-langUl" alt="devider">
+     <li class="ul-lang">css</li>
+     <img src="template5Images/devider-langUl.png" class="devider-langUl" alt="devider">
+ </div>
+ <div class="modal-buttons">
+ <button class="seeLive">See Live <img src="template5Images/Icon.png" alt="seelive"></button>
+ <button class="seeSource">See Source <img src="template5Images/Icon-GitHub.png" alt="seelive"></button>
+</div>
+<div id="overlay"></div>`
+const modalHeader = document.createElement('div')
+modalHeader.innerHTML=`<h6 class="mps">Multi - Post stories</h6>`
+const cancelIcon = document.createElement('img');
+cancelIcon.src = cancelIconInfos.src;
+cancelIcon.alt = cancelIconInfos.alt;
+const Modalopen = () => {
+  header.classList.add('dis-none');
+  main.classList.add('dis-none');
+  footer.classList.add('dis-none');
+  document.body.append(Modalsection);
+  Modalsection.insertBefore(modalHeader, Modalsection.firstChild);
+  modalHeader.classList.add('modal-header')
+  Modalsection.classList.remove('dis-none');
+  Modalsection.classList.add('modal');
+  modalHeader.appendChild(cancelIcon);
+  cancelIcon.classList.add('cancelIcon');
+}
+const Modalclose = () => {
+  header.classList.remove('dis-none');
+  main.classList.remove('dis-none');
+  footer.classList.remove('dis-none');
+  Modalsection.classList.add('dis-none');
+}
+button.forEach(btn => {
+ btn.addEventListener('click',Modalopen);
+});
+cancelIcon.addEventListener('click',Modalclose);
